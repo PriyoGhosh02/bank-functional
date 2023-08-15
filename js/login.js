cfeb22e8da45b50d0c5c0433fac491f0e6223149
prioghosh02 <priyoghosh02@gmail.com>
2023-08-16 02:27:43 +0600

    add login page style

--- js/login.js
+++ js/login.js
@@ -3,16 +3,15 @@ document.getElementById('btn_submit').addEventListener('click', function(){
 // step-2
 const mailField=document.getElementById('user_email');
 const email=mailField.value;
-console.log(email);
 // password
 const passwordField=document.getElementById('user_password');
 const password=passwordField.value;
-console.log(password);
 // Danger Don't try again
 if(email==='priyoghosh02@gmail.com'&& password==='priyo'){
-    console.log('valid')
     window.location.href='bank.html';
 }else{
-    alert('invalid User!!! Your Inputs is Wrong')
+    alert('invalid User!!! Your Inputs is Wrong');
+    mailField.value='';
+    passwordField.value='';
 }
 })
